37bd535029fbf487af5004bb75cf4ac62285d031
okamoto-ryoki <ryoki.okamoto@asia-quest.jp>
2026-01-21 18:51:44 +0900

    docs: add comments to explain logic

--- app/page.tsx
+++ app/page.tsx
@@ -8,47 +8,52 @@ import { useState } from "react";
   }
 
 export default function Home() {
-  const [content, setContent] = useState("");
-  const [time, setTime] = useState("");
-  const [lists, setLists] = useState<NewItem[]>([]);
+  const [content, setContent] = useState(""); //学習内容の入力の一時保存場所
+  const [time, setTime] = useState(""); //学習時間の入力の一時保存場所
 
-  const [error, setError] = useState(false);
+  const [lists, setLists] = useState<NewItem[]>([]);  //上2項目とidの一時保存場所
+
+  const [error, setError] = useState(false); // 入力が誤りであるときのメッセージの表示、非表示
 
   const contentChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setContent(e.target.value);
-  }
+  } //学習内容の入力フォーム
 
   const timeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTime(e.target.value);
-  }
+  } //学習時間の入力フォーム
 
   const handleRegister = (e: React.FormEvent) => {
-    e.preventDefault();
+    e.preventDefault(); //formのsubmitが反応しないように
+
     if (!content || !time) {
       setError(true);
       return
-    };
+    }; //2項目に入力がなかった場合のエラーメッセージの表示
 
     if(isNaN(Number(time))) {
       setError(true);
       return
-    }
+    }  //学習時間の入力フォームに数字以外が入った場合のエラーメッセージの表示
 
-    setError(false);
+    setError(false); //エラーメッセージが出た場合、ここで非表示に
 
     const newItem :NewItem = {
       id: crypto.randomUUID(),
       title: content,
       time: time
-    };
-    const section = [...lists, newItem];
+    };  //新しい入力情報のデータを使いやすく、見やすくする
+
+    const section = [...lists, newItem]; //前までのデータと今入力されたデータの保存
     setLists(section);
+
     setContent("");
-    setTime("");
+    setTime(""); //入力欄に入力した情報が滞留しないために、useStateのリセット
   }
 
   return (
     <div className="flex justify-center items-center flex-col min-h-screen bg-[#101828] p-10">
+      {/* 入力フォーム */}
       <form className="border-[#303b4a] border-2  bg-[#1e2938] rounded-lg max-w-sm w-full px-6 py-7">
         <div className="bg-[#1e2938] mb-4">
           <label className="block bg-[#1e2938] text-sm" htmlFor="content">学習内容</label>
@@ -63,8 +68,9 @@ export default function Home() {
         </div>
         <div className={`bg-[#1e2938] text-red-500 font-bold  ${error ? "visible" : "invisible"}`}>※入力内容を確認してください。</div>
         <button className="bg-[#4f39f6] rounded-lg text-sm w-full px-12 py-3" onClick={(e) => handleRegister(e)}>登録</button>
-      </form>
+      </form> 
 
+      {/* 入力情報が出力される */}
       {lists.length > 0 && 
       <div className="border-2 border-[#303b4a] rounded-lg bg-[#1e2938] max-w-sm w-full m-5 p-6">
         {lists.map((list) => (
